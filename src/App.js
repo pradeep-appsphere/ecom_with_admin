10e032b29d579909d1751931748b449dd7ff70d2
Pradeep Kumar <pradeep@appsphere.in>
2021-03-08 16:34:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import companyLogo from './assets/images/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <br/>
+      <br/>
+      <center>Welcome to</center>
+      <br/>
+        <img src={companyLogo} alt="Best Buy"/>
+    </div>
+  );
+}
+
+export default App;
